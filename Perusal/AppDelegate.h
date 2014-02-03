3fa2fe63661931d68789e9a4fe0b763a8dba3261
Devan Buggay <sbuggay@gmail.com>
2014-02-03 11:12:58 -0600

    Initial Commit

---
+++ Perusal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Perusal
+//
+//  Created by Devan Buggay on 2/3/14.
+//  Copyright (c) 2014 Devan Buggay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Perusal/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Perusal
+//
+//  Created by Devan Buggay on 2/3/14.
+//  Copyright (c) 2014 Devan Buggay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
